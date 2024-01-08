4d02b72fb77c429e69dbe305aebb833df29ee8dd
Russell Mendonca <rmendonc@andrew.cmu.edu>
2024-01-08 02:13:55 -0500

    added vis tool

--- vis_tools.py
+++ vis_tools.py
@@ -1,28 +1,36 @@
 
+import cv2
+import os
 
-# def gen_vid_from_dir(delay = 5):
+def remove_filename_whitespace(directory):
+  for filename in os.listdir(directory):
+    new_filename = filename.replace(' ', '_')
+    os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+    print(f"Renamed '{filename}' to '{new_filename}'")
 
-
-# import cv2
-# import os
-
-def create_video(image_folder, video_name, duration=5, frame_size=(1920, 1080)):
-    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg") or img.endswith(".png")]
+def create_video(image_folder, video_name, duration=3):
+    _files = os.listdir(image_folder)
+    _files.sort()
+    images = [img for img in _files if img.endswith(".jpg") or img.endswith(".png")]
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
 
-    # Video writer
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-    video = cv2.VideoWriter(video_name, fourcc, 1/duration, (width, height))
+    # Video writer with XVID codec
+    fourcc = cv2.VideoWriter_fourcc(*'XVID') 
+    video = cv2.VideoWriter(video_name, fourcc, 1.0, (width, height))
 
     for image in images:
         frame = cv2.imread(os.path.join(image_folder, image))
-        frame = cv2.resize(frame, frame_size)
-        for _ in range(duration):
+        for _ in range(int(duration * 1)):  # Increase the frame rate slightly
             video.write(frame)
 
     cv2.destroyAllWindows()
     video.release()
 
 # Usage
-create_video('path_to_image_directory', 'output_video.mp4')
+if __name__ == '__main__':
+
+  create_video('data/heatmap_jan8/gmmeasy',  'data/heatmap_jan8/gmmeasy_vid.mp4')
+  create_video('data/heatmap_jan8/points_easy',  'data/heatmap_jan8/points_easy_vid.mp4')
+  create_video('data/heatmap_jan8/RGB',  'data/heatmap_jan8/RGB_vid.mp4')
+
